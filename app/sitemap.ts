d911587a311d3653458cddf06a4266c6cb4a4fa4
hamdtrailfive <hamdullah@trailfive.com>
2025-12-09 11:11:06 +0500

    minor fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://mailboiler.com";
+  const baseUrl = "https://www.mailboiler.com";
 
   const routes: {
     path: string;
